0b1a3436178f8dc4e87c362b02b3424ae1e6deba
Chatziefstratiou <gscu@gft.com>
2017-06-01 10:11:06 +0200

    added for rest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  //getUsers = 'http://localhost:8080/projectClient/webservice/user/Json';
+  getUsers='http://localhost:8080/dukes/resources/dukes';
+
+  exampleName = [{ name: 'first', lname: 'second', adress: 'somewhere', postcode: '23', city: 'athens', tel: '1234578' }];
+
+  constructor(private http: Http) {
+
+  }
+
+
+  ngOnInit() {
+    this.http.get(this.getUsers).toPromise().then(r => r.json()).then(r => this.exampleName = r);
+  }
+
 }
